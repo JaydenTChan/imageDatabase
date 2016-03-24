69d253b1f9082ce7d1d7d3f6408df13516960bad
JaydenTChan <jayden.t.chan@gmail.com>
2016-03-23 19:18:13 -0600

    Added new files & php

---
+++ imageupload/php/PHPconnectionDB.php
@@ -0,0 +1,11 @@
+<?php
+function connect(){
+	$conn = oci_connect('jayden1', 'Dragonxp12345t');
+	if (!$conn) {
+		$e = oci_error();
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+
+	return $conn;
+}
+?>
