92ac9ea29be1119e8d29dd8dd2738e79d9cd05ad
JaydenTChan <jayden.t.chan@gmail.com>
2016-03-31 02:25:18 -0600

    Further implemented the groupLoad.php

--- imageupload/php/groupLoad.php
+++ imageupload/php/groupLoad.php
@@ -46,10 +46,6 @@ function getUserOwnerGroups(){
 function getUserGroups(){
 	//This function gets all the groups the user belongs to
 	//This function will return ALL groups to the admin (Admin may view any image)
-}
-
-function createGroup(){
-	//This function is used to create new groups
 	session_start();
 
 	//establish connection
@@ -58,9 +54,11 @@ function createGroup(){
 		$e = oci_error();
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 	}
-
-	$number = rand();
-	$sql = 'INSERT INTO groups values(' . $number . ', \'admin\',\'cool\', to_char(sysdate, \'DD-MON-YYYY\'))';
+	
+	//Get the groups where the owner is part of a group
+	$sql = 'SELECT group_id, group_name FROM group_lists, groups 
+	WHERE friend_id = user_name
+	AND friend_id = \'' . $_SESSION["user"] . '\'';
 
 	//Prepare sql using conn and returns the statement identifier
 	$stid = oci_parse($conn, $sql);
@@ -72,7 +70,59 @@ function createGroup(){
 		$message = "Server busy";
 		echo "<script type='text/javascript'>";
 		echo "alert('$message');";
+		echo "window.location.href = \"../login.html\";";
 		echo "</script>";
+	}else{
+		echo "<select name=\"groupList\">";
+		
+		while($row = oci_fetch_row($stid)){
+			//Loop until no more rows
+			echo "<option value=\"" . $row[0] . "\">" . $row[2] . "</option>";
+		}
+		echo "</select>";
+	}
+
+	
+	// Free the statement identifier when closing the connection
+	oci_free_statement($stid);
+	oci_close($conn);
+	
+	return;
+}
+
+function createGroup(){
+	//This function is used to create new groups
+	session_start();
+	$groupName = $_POST['groupname'];
+
+	//establish connection
+	$conn=connect();
+	if (!$conn) {
+		$e = oci_error();
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+
+	$number = rand();
+	$sql = array(
+	'INSERT INTO groups values(' . $number . ', \'' . $_SESSION["user"] . '\',
+	\'' . $groupName . '\', to_char(sysdate, \'DD-MON-YYYY\'))',
+	'INSERT INTO group_lists values(' . $number . ', \'' . $_SESSION["user"] . '\',
+	to_char(sysdate, \'DD-MON-YYYY\'), null)');
+
+	for ($count = 0 ; $count < 1 ; $count++){
+		//Iterate all sql statements
+		//Prepare sql using conn and returns the statement identifier
+		$stid = oci_parse($conn, $sql[$count]);
+		//Execute a statement returned from oci_parse()
+		$res=oci_execute($stid);
+
+		if (!$res) {
+			//Error Message
+			$message = "Server busy";
+			echo "<script type='text/javascript'>";
+			echo "alert('$message');";
+			echo "</script>";
+		}
 	}
 
 	// Free the statement identifier when closing the connection
@@ -84,6 +134,12 @@ function createGroup(){
 
 function loadGroup(){
 	//This function loads all the information for a specific group so that the owner may edit it.
+	//TODO: READ AND ECHO EDITABLE TEXTBOXES
+}
+
+function saveGroup(){
+	//This function saves the group back to the table with the updated statistics
+	//TODO: READ TEXTBOXES AND UPDATE TABLE
 }
 
 
